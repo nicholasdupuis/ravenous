d6513cf9f23416a4f1af1d7d05bb926d15acae83
Nicholas Dupuis <ndupuis08@gmail.com>
2020-08-14 16:22:23 -0500

    Add yelp api

--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -8,7 +8,7 @@ class BusinessList extends React.Component {
         return (
             <div className="BusinessList">
                 {this.props.businesses.map(biz => {
-                    return <Business business={biz} />
+                    return <Business key={biz.id} business={biz} />
                 })}
             </div>
         );
